e8512ae2598fabb62d48cd39e5d4306c5a19eedc
malvi <mrp92@njit.edu>
2018-10-21 23:23:25 -0400

    Task:Edited PagesController.php.

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController')->name('home');
-
-Route::get('/about', function () {
-    return view('pages.about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('pages.contact');
-})->name('contact');
-Route::post('/contact', function () {
-
-$data = request()->all();
-
-echo "Email: ". $data['email'].'<br>';
-echo "Body:". $data['body'];
-
-
-});
-
-
-
+Route::get('/', 'PagesController@home') ->name('home');
+Route::get('/about','PagesController@about' )->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::post('/contact', 'PagesController@store')->name('contact.store');
+Route::get('/thanks/{name}', 'PagesController@thanks')->name('thanks');
