d1e66a6ba514e323f65326384c1d5a066c1c6522
malvi <mrp92@njit.edu>
2018-10-21 23:32:41 -0400

    Task:Edit home page .

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-class PageController extends Controller{
+class PagesController extends Controller{
     function home(){
         return view('pages.home');
     }
