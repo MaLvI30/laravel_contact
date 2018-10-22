f9345e318946318dd070ff66c51482c514a08383
malvi <mrp92@njit.edu>
2018-10-21 23:53:56 -0400

    Task:Edit about.blade.php and contact.blade.php file .

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,7 @@
 @section('content')
 
         <h1>This is my About Page.</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-
+        <p class="lead"> This is a web form.</p>
 @endsection
 
 
